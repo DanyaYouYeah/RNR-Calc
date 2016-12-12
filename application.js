b4695badf7aea2965e5f5bc4aaddf1f039f0d61c
Daniil Demidenko <danilka95v@gmail.com>
2016-12-12 19:25:50 +1000

    interfaceUpdate

--- application.js
+++ application.js
@@ -1,29 +1,30 @@
 defCaption = "Введите кол-во ₽";
+defCaption2 = "Введите кол-во"
 Vue.use(VueMaterial)
 
-var firstQuestionsArray = ['Test', 'Ground', '', '', ''];
+var firstQuestionsArray = ['Оклад', 'Оплата за доставку', 'Бензин', 'Питание', 'Сотовая Связь'];
 var firstQuestionsQuantity = firstQuestionsArray.length;
 var firstQuestionNumbers = ["1", "2", "3", "4", "5"];
-var firstQuestionDiscription = ['Under', '', '', '', ''];
-var firstQuestionExample = ['', 'Ha', '', '', ''];
+var firstQuestionDiscription = ['Сколько вы платите одному курьеру за смену. Если он получает раз в месяц то поделите на кол-во дней', 'Какую сумму вы даете курьеру за выполненый заказ, помимо или вместо оклада', 'Сколько денег в среднем вы даете на бензин. Посчитайте сумму в день', 'Какую сумму в день, вы тратите на одного курьера', 'Сколько вы даете денег курьеру на сотовую связь в день. Если вы даете на месяц, то разделите на кол-во рабочих дней'];
+var firstQuestionExample = ['(Например: 30000 рублей / 30 дней = 1000 рублей за смену)', '', '(Например: 4000 рублей / 10 дней = 400 рублей в день. Или 3 рубля/км = 300 рублей)', '', '(Например: 500 рублей / 15 дней = 33.3 рубля / день)'];
 var firstAnwsersArray = [];
 
-var secondQuestionsArray = ['', '', '', '', '', '', ''];
+var secondQuestionsArray = ['Объявления', 'Налоги', 'Термосумки', 'Обучение', 'Диспетчер', 'Мотивация', 'Обслуживание Авто'];
 var secondQuestionsQuantity = secondQuestionsArray.length;
 var secondQuestionNumbers = ["1", "2", "3", "4", "5", "6", "7"];
-var secondQuestionDiscription = ['', '', '', '', '', '', ''];
-var secondQuestionExample = ['', '', '', '', '', '', ''];
+var secondQuestionDiscription = ['Сколько вы тратите денег в месяц на объявления, чтобы привлечь курьеров.', 'Сколько вы тратите на курьеров в квартал', 'Сколько стоят ваши термосумки и какое кол-во. Средняя жизнь данной сумки 1 год. Давайте посчитаем сколько вы тратите на них в день.', 'Сколько стоит час вашего сотрудника, который обучает курьеров? Он ведь мог потрпатить это время на что то другое. Давайте посчитаем.', 'Ввод в штат сотрудника, который контролирует работу курьера.', 'Премиальный фонд на сотрудника за выполнение плана', 'Если в вашей компании есть корпоративные машины, на которых вы осувстляете доставку, то это мы тоже нужно посчитать'];
+var secondQuestionExample = ['Например: 20000 рублей/месяц', 'Например: 10000 рублей / 120 дней', 'Например: 5 сумок по 3000 рублей / 365 дней = 41 рубль / день', 'Например: 30000 рублей оклад. 8 часов рабочая смена. 22 смены. Следовательно 30000 рублей / 22 смены / 8 часов = 170 рублей/час. На обучение мы тратим 3 часа в неделю. 170 рублей * 3 часа * 4 недели =  2040 рублей/месяц. Чтобы посчитать сколько это в день делаем простой подсчет 2040 рублей / 30 дней = 68 рублей.', 'Например: 30000 рублей / 30 дней = 1000 рублей/смена', 'Например: 5000 рублей / 30 дней = 166 рублей / день', 'Например: 4 машины * 40000 рублей / год = 160000 рублей / год. 160000 рублей / 365 дней = '];
 var secondAnwsersArray = [];
 
-var thirdQuestionsArray = ['', '', ''];
+var thirdQuestionsArray = ['Не выполненые заказы', 'Простой курьера', 'Не выход на работу курьера'];
 var thirdQuestionsQuantity = thirdQuestionsArray.length;
 var thirdQuestionNumbers = ["1", "2", "3"];
-var thirdQuestionDiscription = ['', '', ''];
-var thirdQuestionExample = ['', '', ''];
+var thirdQuestionDiscription = ['Нужно указать сумму, которую вы потеряли по причине того, что не смогли отвезти заказа из-за отсутствия курьера, или длительного ожидания заказа.', 'Если вы платите курьеру оклад или за выход, то он должен отрабатывать эти деньги. Тут нужно указать сумму которую вы тратите, на то что курьер просто сидит и ждет заказ. ', 'Бывает ли такое что ваш курьер по какой-то причине просто не пришел на работу. Сколько вы из-за этого теряете денег? '];
+var thirdQuestionExample = ['Например: 2 заказа в день по 1500 рублей = 3000 рублей / день.', 'Например: 1000 рублей смена / 10 часов = 100 ', 'Например: не выполнено 5 заказов по 1500 рублей = 7500 рублей/день).'];
 var thirdAnwsersArray = [];
 
 
-var fourthQuestionsArray = ['', ''];
+var fourthQuestionsArray = ['Сколько в среднем курьеров в смену', 'Среднее кол-во заказов в день?'];
 var fourthQuestionsQuantity = fourthQuestionsArray.length;
 var fourthQuestionNumbers = ["1", "2"];
 var fourthQuestionDiscription = ['', ''];
@@ -124,7 +125,7 @@ var secondTest = new Vue({
     discriptionReady: true,
     exampleReady: true,
     anwsersQuantity: 0,
-    inputValue: 0
+    inputValue: ''
   },
   methods:{
      getNextData: function () {
@@ -198,7 +199,7 @@ var thirdTest = new Vue({
     discriptionReady: true,
     exampleReady: true,
     anwsersQuantity: 0,
-    inputValue: 0
+    inputValue: ''
   },
   methods:{
      getNextData: function () {
@@ -266,13 +267,13 @@ var fourthTest = new Vue({
     currentQuestion: fourthQuestionsArray[0],
     questionDiscription: fourthQuestionDiscription[0],
     questionExample: fourthQuestionExample[0],
-    defaultCaption: defCaption,
+    defaultCaption: defCaption2,
     currentValue: 0,
     show: true,
     discriptionReady: true,
     exampleReady: true,
     anwsersQuantity: 0,
-    inputValue: 0
+    inputValue: ''
   },
   methods:{
      getNextData: function () {
@@ -331,7 +332,30 @@ var fourthTest = new Vue({
   }
 })
 
+Vue.directive('focus', {
+    bind: function () {
+        var object = this.el;
+        Vue.nextTick(function() {
+            object.focus();
+        });
+    }
+});
+
 
 var getResults = new Vue({
   el: '#getResults',
+  data:{
+  show: false,
+},
+  methods:{
+    getResults: function (){
+      if (!this.show){
+       this.show = true;
+      } else {
+        this.show = false;
+      }
+
+    console.log("OK");
+    }
+  }
 })
